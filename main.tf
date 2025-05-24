bce2675994b5f2876175895c6472767849125b94
Diwas Gautam <gautamdiwas707@gmail.com>
2025-05-24 16:11:23 -0400

    fix tf format

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 terraform {
-    required_providers {
-        okta = {
-            source = "okta/okta"
-        }
-        vault = {
-            source  = "hashicorp/vault"
-            version = "~> 3.20"
-        }
+  required_providers {
+    okta = {
+      source = "okta/okta"
     }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 3.20"
+    }
+  }
 }
 
 data "vault_kv_secret_v2" "okta_private_key" {
@@ -16,20 +16,20 @@ data "vault_kv_secret_v2" "okta_private_key" {
 }
 
 provider "vault" {
-    address = "http://127.0.0.1:8200"
-    token = var.vault_token
-  
+  address = "http://127.0.0.1:8200"
+  token   = var.vault_token
+
 }
 
 
 provider "okta" {
-  org_name = "dev-1173502"
-  base_url = "okta.com"
+  org_name  = "dev-1173502"
+  base_url  = "okta.com"
   client_id = var.okta_client_id
   #private_key = data.vault_kv_secret_v2.okta_private_key.data["key"]
   private_key = var.okta_private_key
-  scopes = ["okta.groups.manage"]
- 
+  scopes      = ["okta.groups.manage"]
+
 }
 
 
